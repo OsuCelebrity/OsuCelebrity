1779d79175290fce29c324f427fa5b59fe3ef30c
Redback93 <Redback93@hotmail.com>
2014-08-20 08:24:53 +0800

    Initial Commit

---
+++ src/me/reddev/OsuCelebrity/OsuCelebrity.java
@@ -0,0 +1,20 @@
+package me.reddev.OsuCelebrity;
+
+public class OsuCelebrity
+{
+
+	public void run()
+	{
+		
+	}
+	
+	/**
+	 * @param args Command line arguments
+	 */
+	public static void main(String[] args)
+	{
+		OsuCelebrity mainBot = new OsuCelebrity();
+		mainBot.run();
+	}
+
+}
