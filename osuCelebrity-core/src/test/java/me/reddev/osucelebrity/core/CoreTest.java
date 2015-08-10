71dd1c26ee6b3430c02ebd3d8b0c517ae5ef0b07
Tillerino <tillmann.gaida@gmail.com>
2015-08-10 20:32:31 +0200

    Fixed timing issue in CoreTest (wasn't actually failing, but who knows)

--- osuCelebrity-core/src/test/java/me/reddev/osucelebrity/core/CoreTest.java
+++ osuCelebrity-core/src/test/java/me/reddev/osucelebrity/core/CoreTest.java
@@ -1,5 +1,7 @@
 package me.reddev.osucelebrity.core;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -10,10 +12,15 @@ import me.reddev.osucelebrity.twitch.Twitch;
 import org.junit.Test;
 import org.tillerino.osuApiModel.OsuApiUser;
 
+import java.util.concurrent.CountDownLatch;
+
 public class CoreTest {
   @Test(timeout = 1000)
   public void testQueueSimple() throws Exception {
     Osu osu = mock(Osu.class);
+    CountDownLatch latch = new CountDownLatch(1);
+    doAnswer(x -> { latch.countDown(); return null; }).when(osu).startSpectate(any());
+
     Twitch twitch = mock(Twitch.class);
     CoreSettings settings = mock(CoreSettings.class);
     Core core = new Core(osu, twitch, settings);
@@ -23,7 +30,9 @@ public class CoreTest {
     OsuApiUser user = mock(OsuApiUser.class);
     QueueSelfOsuCommand command = mock(QueueSelfOsuCommand.class);
     when(command.getUser()).thenReturn(user);
+    
     core.handleOsuCommand(command);
+    latch.await();
     verify(osu).startSpectate(user);
     thread.interrupt();
     thread.join();
